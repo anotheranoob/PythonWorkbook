e93f86c411c3bd21c94987e5ff22d44587336e04
anotheranoob <k447038@gmail.com>
2017-09-28 22:12:38 -0400

    Add files via upload

---
+++ p6 r23 wu kevin exer 02.py
@@ -0,0 +1,59 @@
+"""
+Name:  Wu Kevin, Period 6
+Program:  Python Workbook 002
+
+This program says hi to the user.
+
+1.  Significant constants:
+
+2.  Imports:
+
+3.  The inputs are:
+yourName
+
+3.  Computations:
+
+4.  The outputs are:
+"Hello" + yourName
+
+"""
+
+################
+################
+################
+# Significant
+# constants
+
+
+################
+################
+################
+# Imports
+
+################
+################
+################
+# Inputs
+yourName=input("What is your name?:  ")
+
+################
+################
+################
+# Computations
+
+
+
+################
+################
+################
+# Output
+print("Hello " + yourName + "!")
+
+
+
+################
+################
+################
+# Terminate
+# Program
+str(input("Press ENTER to quit."))   		  # required
---
+++ p6 r23 wu kevin exer 04.py
@@ -0,0 +1,61 @@
+"""
+Name:  Charlie Brown, Period 9
+Program:  Page 999, Program 999
+
+This program calculates the area of
+a field.
+
+1.  Significant constants:
+    ACRES_IN_FEET=1/413560
+
+2.  Imports:
+
+3.  The inputs are:
+    width
+    length
+
+3.  Computations:
+    area=width*length*FEET_IN_ACRE
+
+4.  The outputs are:
+    area
+
+"""
+
+################
+################
+################
+# Significant
+# constants
+ACRES_IN_FEET=1/43560
+
+################
+################
+################
+# Imports
+
+################
+################
+################
+# Inputs
+width=int(input("What is the width of the field in feet?:  "))
+length=int(input("What is the length of the field in feet?:  "))
+
+################
+################
+################
+# Computations
+area=width*length*ACRES_IN_FEET
+
+################
+################
+################
+# Output
+print("The area of the field in acres is {}".format(area))
+
+################
+################
+################
+# Terminate
+# Program
+str(input("Press ENTER to quit."))   		  # required
